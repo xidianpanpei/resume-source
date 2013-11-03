4c95b35081dcb987e8db7a0ea814b8a4062dc8dd
panpei <xidianpanpei@126.com>
2013-11-04 00:57:19 +0800

    1.增加一键编译脚本 2.修改部分简历内容

---
+++ clean.bat
@@ -0,0 +1,6 @@
+del *.aux /s
+del *.dvi /s
+del *.log /s
+del *.out /s
+del *.ps /s
+del *.pdf /s
---
+++ make.bat
@@ -0,0 +1,16 @@
+@echo off
+title Resume Template
+
+call "./clean.bat"
+
+echo "===============>BEGAIN<==============="
+latex my_cv_zh.tex
+dvips my_cv_zh.dvi
+ps2pdf my_cv_zh.ps
+
+latex my_cv_en.tex
+dvips my_cv_en.dvi
+ps2pdf my_cv_en.ps
+echo "================>END<================="
+
+pause
